13efe39fb334b23fc47d0ed1526ee578a578d8b4
cutik <vitaliy.boldin@gmail.com>
2016-03-17 14:11:14 +0200

    added new tool-buttons style

---
+++ scripts/tools-scripts.js
@@ -0,0 +1,13 @@
+var addTextBox = document.querySelector('#addTextBox');
+var textBoxCounter = 0;
+
+addTextBox.addEventListener("click", function() {
+  // var textBox = document.createElement("div");
+  // textBox.setAttribute("contenteditable", "");
+  // var current = "textbox" + textBoxCounter;
+  // textBox.id = current;
+  // textBox.innertext = "azazaza";
+  // textBoxCounter++;
+  //
+  // document.getElementById("tools-panel").appendChild(textBox);
+})
